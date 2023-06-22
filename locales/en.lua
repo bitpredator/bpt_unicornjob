4d4d420af96a181de978318923e9ad81043ec24c
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-22 09:24:01 +0200

    chore: initial commit

---
+++ locales/en.lua
@@ -0,0 +1,36 @@
+Locales['en'] = {
+  -- Cloakroom
+  ['cloakroom'] = 'cloakroom',
+  ['wear_citizen'] = 'civilian clothes',
+  ['barman_outfit'] = 'bartender outfit',
+  ['dancer_outfit_1'] = 'dance outfit',
+  ['no_outfit'] = 'there\'s no uniform to fit you...',
+  ['open_cloackroom'] = 'press ~INPUT_CONTEXT~ to change',
+  -- Vehicles  
+  ['vehicle_menu'] = 'vehicle',
+  ['vehicle_out'] = 'there is already a vehicle outside',
+  ['vehicle_spawner'] = 'press ~INPUT_CONTEXT~ to exit a vehicle',
+  ['store_vehicle'] = 'press ~INPUT_CONTEXT~ to store the vehicle',
+  ['service_max'] = 'full service: ',
+  ['spawn_point_busy'] = 'a vehicle is near the exit point',
+  -- Boss Menu  
+  ['take_company_money'] = 'remove money company',
+  ['deposit_money'] = 'deposit money',
+  ['amount_of_withdrawal'] = 'amount of withdrawal',
+  ['invalid_amount'] = 'invalid amount',
+  ['amount_of_deposit'] = 'deposit amount',
+  ['open_bossmenu'] = 'press ~INPUT_CONTEXT~ to open the menu',
+  -- Billing Menu  
+  ['billing'] = 'bill',
+  ['no_players_nearby'] = 'no players nearby',
+  ['billing_amount'] = 'invoice amount',
+  ['amount_invalid'] = 'invalid amount',
+  -- Misc  
+  ['map_blip'] = 'vanilla Unicorn',
+  ['unicorn'] = 'vanilla Unicorn',
+  -- Teleporters
+  ['e_to_enter_1'] = 'press ~INPUT_PICKUP~ to go behind the bar',
+  ['e_to_exit_1'] = 'press ~INPUT_PICKUP~ to go in front off the bar',
+  ['e_to_enter_2'] = 'press ~INPUT_PICKUP~ to go up to the roof.',
+  ['e_to_exit_2'] = 'press ~INPUT_PICKUP~ to go down to the offices.',
+}
